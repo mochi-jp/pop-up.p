a3451147c429cd67c74a39cef9d12d27fc975513
mochi-jp <the_count_of_chocolate@yahoo.co.jp>
2025-05-09 06:54:36 +0900

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -3,12 +3,12 @@ self.addEventListener("install", function(e) {
   e.waitUntil(
     caches.open("mochi-calculator").then(function(cache) {
       return cache.addAll([
-        "/pop-up.p/",
-        "/pop-up.p/index.html",
-        "/pop-up.p/manifest.json",
-        "/pop-up.p/service-worker.js",
-        "/pop-up.p/icon-192.png",
-        "/pop-up.p/icon-512.png"
+       "/pop-up.p/",
+       "/pop-up.p/index.html",
+       "/pop-up.p/manifest.json",
+       "/pop-up.p/service-worker.js",
+       "/pop-up.p/icon-192.png",
+       "/pop-up.p/icon-512.png"
         // 他にCSSやJSファイルがあるならここに追加
       ]);
     })
